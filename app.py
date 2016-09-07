f1ac1c0b840cf2135702027515036b23bde501de
Nick Such <nicksuch@gmail.com>
2016-09-07 11:53:22 -0400

    Add menu for user

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
 # Upper-Polo To Do List App
+
+from item import Item
+from item_list import ItemList
+
+def main():
+    while True:
+        # Display menu
+        print("1. Add Item")
+        print("2. Remove Item")
+        print("3. List Items")
+        print("0. Exit")
+        
+        # Get user input
+        user_in = int(input())
+        
+        # Add item
+        if user_in == 1:
+            print("Add Item")
+        
+        # Remove item
+        elif user_in == 2:
+            print("Remove Item")
+        
+        # Show items in item_list
+        elif user_in == 3:
+            print("Add Item")
+
+        # Exit program
+        elif user_in == 0:
+            exit(0)
+
+        else:
+            print("invalid menu option")
+
+main()
--- item_list.py
+++ item_list.py
@@ -3,13 +3,13 @@
 # An Item object for each user entry to their todo list.
 
 class ItemList:
-	def __init__(self):
+    def __init__(self):
         self.userlist = []
 
     def add(self, item):
         # TODO: Check inputs
 
-        userlist.append(item)
+        self.userlist.append(item)
         
 
     def remove(self, item):
