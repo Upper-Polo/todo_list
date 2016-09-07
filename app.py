7e722cc27684a9642408877930b6f1a69e77dc1c
jasoncwilley <jason.c.willey@gmail.com>
2016-09-07 14:08:24 -0400

    fix bug 13

--- app.py
+++ app.py
@@ -14,26 +14,26 @@ def main():
         print("0. Exit")
         
         # Get user input
-        user_in = int(input())
+        user_in = input()
         
         # Add item
-        if user_in == 1:
+        if user_in == "1":
             print("Add Item")
             my_item = input("Please enter To-Do item\n")
             my_list.add(my_item)
             print(my_list.userlist[-1])
         
         # Remove item
-        elif user_in == 2:
+        elif user_in == "2":
             print("Remove Item")
         
         # Show items in item_list
-        elif user_in == 3:
+        elif user_in == "3":
             print("Add Item")
 
         # Exit program
-        elif user_in == 0:
-            exit(0)
+        elif user_in == "0":
+            exit("0")
 
         else:
             print("invalid menu option")
