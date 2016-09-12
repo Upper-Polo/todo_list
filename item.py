0089402de002501f9a7c6437ca33b942444cee51
jasoncwilley <jaycw0001@gmail.com>
2016-09-11 22:57:32 -0400

    prints date/time stamp alongside done when item is marked complete

--- item.py
+++ item.py
@@ -4,16 +4,16 @@
 # Each new todo entry is an instance of Item.
 # Item accepts a title, which is the text for the entry.
 # Item has a 'done' value for marking as complete.
-
+import datetime
 class Item:
     def __init__(self, title):
         self.title = title
         self.done = False
-        # TODO: self.date
+        self.timestamp = str(datetime.datetime.now()).split('.')[0]
         # TODO: self.description
 
     def print_item(self):
         if self.done:
-            return '{} (DONE)'.format(self.title)
+            return '{}  (DONE)   {}'.format(self.title, self.timestamp)
         else:
             return '{}'.format(self.title)
